3ee9b855c0977fac40353e038bb49e65ec9e5f84
Loraelize <lauraelisamerino@gmail.com>
2022-05-14 18:00:10 -0400

    Se crea función fibonacci(n) y se utiliza en el endpoint

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/fibonacci/<int:num>")
 def endpoint_fibonacci(num):
-    return str(num)
+    return str(fibonacci(num))
+
+
+def fibonacci(n):
+    if n < 2:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
